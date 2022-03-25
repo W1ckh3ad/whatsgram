179c5618a06e01b1f42e4ee81e1bd9ff5264bff2
Ac1dR41n <69206569+Ac1dR41n@users.noreply.github.com>
2022-03-25 17:03:22 +0100

    Update change-profile.spec.js

--- cypress/integration/auth/change-profile.spec.js
+++ cypress/integration/auth/change-profile.spec.js
@@ -11,74 +11,94 @@ describe("change user settings", () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:8100/sign-in')
-  });
 
-  it ('has a title', () => {
-    cy.contains('Login')
-  });
+    ///Login
+    cy.get('input[name="email"]').clear()
 
-    /// normal login
     cy.get('input[name="email"]').type('jannik.decker@edu.fhdw.de')
       .should('have.value', 'jannik.decker@edu.fhdw.de')
-    
+          
+    cy.get('input[name="Password"]').clear()
+        
     cy.get('input[name="Password"]').type('123456')
-  
-    cy.contains('button', 'Login').click()
+        
+    cy.get('ion-button[type="submit"]').click()    
+
   });
 
-    cy.contains('settings').click()
+  afterEach(() => {
+    cy.get('[id="tab-button-settings"]').click()
+
+    cy.contains('Sign Out').click()
+
+  });
   
+
 ///Change Name
-  it ('has a title', () => {
-    cy.contains('Profil Bearbeiten')
-  });  
+  it('should change name', () => {
+    cy.get('[id="tab-button-settings"]').click()
+    
+    cy.contains('Profil bearbeiten').click()
+
+    cy.get('input[name="displayName"]').clear()
 
     cy.get('input[name="displayName"]').type('Jannik.Testname')
       .should('have.value', 'Jannik.Testname')
     
-    cy.contains('button', 'Speichern').click()
+    cy.contains('Speichern').click()
 
-  ///Change Telephon Number
-  it ('has a title', () => {
-    cy.contains('Profil Bearbeiten')
-  });  
+    cy.go('back')
+
+  });
+
+  ///Change Telephone Number
+  it('should change phone number', () => {
+    cy.get('[id="tab-button-settings"]').click()
+    
+    cy.contains('Profil bearbeiten').click()
+
+    cy.get('input[name="phoneNumber"]').clear()
 
     cy.get('input[name="phoneNumber"]').type('+49 12345678')
       .should('have.value', '+49 12345678')
     
-    cy.contains('button', 'Speichern').click()
+    cy.contains('Speichern').click()
 
+    cy.go('back')
+     
+  }); 
 
 
 ///Change Profil Picture URL
-  it ('has a title', () => {
-    cy.contains('Profil Bearbeiten')
-  });  
-
-    cy.get('input[name="photoURL"]').type('https://th.bing.com/th/id/R.1c88c61f3481d30aec5d390fd0b51d1f?rik=KmquqDLyP%2fHPTw&pid=ImgRaw&r=0')
-      .should('have.value', 'https://th.bing.com/th/id/R.1c88c61f3481d30aec5d390fd0b51d1f?rik=KmquqDLyP%2fHPTw&pid=ImgRaw&r=0')
+  it('should change photo', () => {
+    cy.get('[id="tab-button-settings"]').click()
     
-    cy.contains('button', 'Speichern').click()
+    cy.contains('Profil bearbeiten').click()
+ 
+    cy.get('input[name="photoURL"]').clear()
 
+    cy.get('input[name="photoURL"]').type('abcd')
+      .should('have.value', 'abcd')
+    
+    cy.contains('Speichern').click()
+
+    cy.go('back')
+  }); 
 
 ///Change Profil Description
-  it ('has a title', () => {
-    cy.contains('Profil Bearbeiten')
-  });  
+  it('should change description', () => {
+    cy.get('[id="tab-button-settings"]').click()
+    
+    cy.contains('Profil bearbeiten').click()
 
-    cy.get('input[name="description"]').type('Das ist eine Testbeschreibung')
+    cy.get('textarea[name="description"]').clear()
+
+    cy.get('textarea[name="description"]').type('Das ist eine Testbeschreibung')
       .should('have.value', 'Das ist eine Testbeschreibung')
     
-    cy.contains('button', 'Speichern').click()
+    cy.contains('Speichern').click()
 
-/// Back To Settings
-    cy.contains('button', 'Arrow Back').click()
-
- ///Sign Out
-  it ('has a title', () => {
-    cy.contains('Einstellungen')
-  });  
-    cy.contains('button', 'Sign Out').click()
-
+    cy.go('back')
+  }); 
 
 })
